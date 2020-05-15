a02c7c0d583261572bd7b7fb3742a1645d35cdba
lklk1024 <32770832+lklk1024@users.noreply.github.com>
2020-05-15 14:57:11 +0900

    Add files via upload

---
+++ pro11-spring-boot-maven-project/src/main/java/com/tomato/spring/boot/SpringBootMainType.java
@@ -0,0 +1,10 @@
+package com.tomato.spring.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//将当前类标注为一个springboot应用
+@SpringBootApplication
+public class SpringBootMainType {
+	main
+
+}
